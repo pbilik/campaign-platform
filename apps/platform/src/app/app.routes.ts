7b5ef257d963230c54d708a4e36fd23355991adc
Pavel Bilík <pavel.bilik@ibillboard.com>
2024-12-13 15:08:18 +0100

    microfrontend added

--- apps/platform/src/app/app.routes.ts
+++ apps/platform/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'microfrontend',
+    loadChildren: () =>
+      import('microfrontend/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
