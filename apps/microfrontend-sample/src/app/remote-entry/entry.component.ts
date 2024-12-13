90d932e2c6ee4849014890bf8a6ca32c50dc5caa
Pavel Bilík <pavel.bilik@ibillboard.com>
2024-12-13 15:43:01 +0100

    microfrontend is still not shown

--- apps/microfrontend-sample/src/app/remote-entry/entry.component.ts
+++ apps/microfrontend-sample/src/app/remote-entry/entry.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [CommonModule, NxWelcomeComponent],
+  imports: [CommonModule],
   selector: 'app-microfrontend-entry',
-  template: `<app-nx-welcome></app-nx-welcome>`,
+  template: `Microfrontend content`,
 })
 export class RemoteEntryComponent {}
--- apps/platform/src/main-page/main-page.component.ts
+++ apps/platform/src/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'main-page',
+  standalone: true,
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
